5692187944863951918147f1c4ec2ed66f243328
FantastiCole <coleamurphy@gmail.com>
2015-11-10 15:18:09 -0500

    Remove carousel functions, make .current_page_parent's bottom margin responsive, edit comments, style nav LIs.

--- page.php
+++ page.php
@@ -19,7 +19,7 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<!-- Page Name -->
+				<!-- Page Name & Content-->
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 				<?php
