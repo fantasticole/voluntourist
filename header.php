921bf450031ed1ffe18ae54e92f2e54c5ceeec78
FantastiCole <coleamurphy@gmail.com>
2015-11-09 20:42:49 -0500

    Remove nav 'menu-id', add new blue to color scheme, style nav menu children.

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 	<header id="masthead" class="site-header" role="banner">
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span></span><span></span><span></span></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary') ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
