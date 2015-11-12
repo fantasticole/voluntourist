eb74d0415e4fa69ee9e80328e9a0f07332fba9ce
FantastiCole <coleamurphy@gmail.com>
2015-11-12 16:57:50 -0500

    Style images in posts & flags, show post category sub-menu, on single post page, stick footer to bottom, format posts in list.

--- js/voluntourist.js
+++ js/voluntourist.js
@@ -2,11 +2,49 @@ jQuery(document).ready(function($) {
     // Inside of this function, $() will work as an alias for jQuery()
     // and other libraries also using $ will not be accessible under this shortcut
 
-    if ($(window).width() < 601 & $('.current_page_parent').length > 0){
-        var childLIs = $('.current_page_parent .children')[0].children;
-        var margin = 28*childLIs.length;
-        $(".current_page_parent").css("margin-bottom", margin);
+    var parents = {};
+    var menuItems = document.getElementsByClassName('page_item_has_children');
+    for (var x = 0; x < menuItems.length; x++){
+        var current = menuItems[x];
+        var page = current.childNodes[0].innerHTML.toLowerCase();
+        var classes = current.className.split(' ');
+        parents[page] = classes[1];
+        // console.log(parents);
+    };
+
+    if ($('.single-post').length > 0){
+        // Get posts classes.
+        var classes = $('article')[0].className.split(' ');
+        for (var name in classes){
+            if (classes[name].indexOf('category-') !== -1){
+                // Get posts categories from classes.
+                var category = classes[name].slice(classes[name].indexOf('-')+1);
+                // If the class is a parent page...
+                if (parents[category]){
+                    // Show parent's sub-menu.
+                    $('.' + parents[category]).addClass('current_page_parent');
+                    // expandParent();
+                }
+            }
+        }
     }
+
+    function expandParent(){
+        if ($(window).width() < 601 & $('.current_page_parent').length > 0){
+            var childLIs = $('.current_page_parent .children')[0].children;
+            var margin = 28*childLIs.length;
+            $(".current_page_parent").css("margin-bottom", margin);
+        }
+    };
+
+    $('.single-post .itemBody figure:odd').css({
+        'float': 'right',
+        'margin-left' : '30px'
+    });
+    $('.single-post .itemBody figure:even').css({
+        'float': 'left',
+        'margin-right' : '30px'
+    });
 
 	// //Set font-size based on container size.
 	// $.fn.textfill = function() {
@@ -30,6 +68,8 @@ jQuery(document).ready(function($) {
  //    }
 
  //    $('.adjust').textfill();
+
+    expandParent();
 });
 
 
