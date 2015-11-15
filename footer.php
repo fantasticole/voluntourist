6038e5b16b04b7f3c5e73d1a40e61a994feae360
FantastiCole <coleamurphy@gmail.com>
2015-11-15 13:45:54 -0500

    Add social links and remove Wordpress link in footer.

--- footer.php
+++ footer.php
@@ -15,9 +15,14 @@
 
 	<footer id="footer" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'voluntourist' ) ); ?>"><?php printf( esc_html__( 'Powered by %s', 'voluntourist' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( '© 2015 %2$s.', 'voluntourist' ), 'voluntourist', '<a href="http://fantasticole.github.io/" rel="designer" target="_blank">fantasticole</a>' ); ?>
+			<div class="social">
+				<a href="https://www.facebook.com/pages/The-Voluntourist/152316794838183" target="_blank"><i class="fa fa-facebook-square"></i> Facebook</a>
+				<a href="http://www.youtube.com/results?search_query=iamthevoluntourist" target="_blank"><i class="fa fa-youtube-square"></i> YouTube</a>
+				<a href="https://instagram.com/iamthevoluntourist/" target="_blank"><i class="fa fa-instagram"></i> Instagram</a>
+			</div>
+			<div class="copyright">
+				<?php printf( esc_html__( '© 2015 %2$s.', 'voluntourist' ), 'voluntourist', '<a href="http://fantasticole.github.io/" rel="designer" target="_blank">fantasticole</a>' ); ?>
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
