bf50708f40c818f79e4c39b423773189c785a0cd
FantastiCole <coleamurphy@gmail.com>
2015-11-17 13:11:48 -0500

    Restructure footer.

--- footer.php
+++ footer.php
@@ -16,9 +16,9 @@
 	<footer id="footer" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<div class="social">
-				<a href="https://www.facebook.com/pages/The-Voluntourist/152316794838183" target="_blank"><i class="fa fa-facebook-square"></i> Facebook</a>
-				<a href="http://www.youtube.com/results?search_query=iamthevoluntourist" target="_blank"><i class="fa fa-youtube-square"></i> YouTube</a>
-				<a href="https://instagram.com/iamthevoluntourist/" target="_blank"><i class="fa fa-instagram"></i> Instagram</a>
+				<a href="https://www.facebook.com/pages/The-Voluntourist/152316794838183" target="_blank"><i class="fa fa-facebook-square"></i><span class="socialLink"> Facebook</span></a>
+				<a href="http://www.youtube.com/results?search_query=iamthevoluntourist" target="_blank"><i class="fa fa-youtube-square"></i><span class="socialLink"> YouTube</span></a>
+				<a href="https://instagram.com/iamthevoluntourist/" target="_blank"><i class="fa fa-instagram"></i><span class="socialLink"> Instagram</span></a>
 			</div>
 			<div class="copyright">
 				<?php printf( esc_html__( '© 2015 %2$s.', 'voluntourist' ), 'voluntourist', '<a href="http://fantasticole.github.io/" rel="designer" target="_blank">fantasticole</a>' ); ?>
