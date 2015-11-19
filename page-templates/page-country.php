b828745ba728496b5390b99b4cff57c4cc807fe5
FantastiCole <coleamurphy@gmail.com>
2015-11-19 14:36:08 -0500

    Add and style dropdown list to country page, hide volunteer submenu in nav.

--- page-templates/page-country.php
+++ page-templates/page-country.php
@@ -16,6 +16,19 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main child" role="main">
 			<div id="country">
+				<div class="countryList">
+					<strong>WHY VOLUNTEER IN</strong>
+					<form action="<?php bloginfo('url'); ?>" method="get">
+					<?php
+						$current = ( is_page() ) ? $post->ID : 0;
+						$args = array(
+							'child_of' => 12,
+							'selected' => $current,
+							'echo' => 0);
+						echo str_replace('<select ', '<select onchange="this.form.submit()" ', wp_dropdown_pages( $args ));
+					?>
+					</form>
+				</div>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
