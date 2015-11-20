2bdd65d91d3fe2c89bded9f48cfd871d53d552b6
FantastiCole <coleamurphy@gmail.com>
2015-11-20 15:39:05 -0500

    Add most recent article from each category to home page.

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -26,19 +26,45 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+			<div id="features">
 
-				<!-- Page Name -->
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			<!-- Get categories -->
+			<?php
+				$args = array(
+				'child_of'           => 12,
+				'title_li'           => __( '' ),
+			    );
+			    $first = get_categories( $args );
+			?>
+			<?php
+				$args = array(
+				'child_of'           => 13,
+				'title_li'           => __( '' ),
+			    );
+			    $second = get_categories( $args );
+				$categories = array_merge($first, $second);
+			?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
+			<!-- Get content -->
+			<?php foreach ($categories as $cat) {
+				$name = $cat->name;
+				echo '<div class="feature">';
+				echo '<h3>'.$name.'</h3><h4 class="title">';
+				$slug = $cat->slug;
+				$args = array(
+					'category_name' => $slug,
+					'posts_per_page' => 1
+				);
+				query_posts( $args );
+				while ( have_posts() ) : the_post();
+					echo the_title();
+					echo '</h4>';
+					echo the_content('read more');
+				endwhile;
+				echo '</div>';
+			} ?>
 
-			<?php endwhile; // End of the loop. ?>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
